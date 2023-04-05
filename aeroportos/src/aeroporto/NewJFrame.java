b23daf3cabd6fdc33cf58f8ef73a6bca3d32820b
Guilherme Girata <guilherme_girata@hotmail.com>
2023-04-05 18:00:15 -0300

    Aeroportos

---
+++ aeroportos/src/aeroporto/NewJFrame.java
@@ -0,0 +1,343 @@
+package aeroporto;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+
+public class NewJFrame extends javax.swing.JFrame implements ActionListener {
+
+    public NewJFrame() {
+        initComponents();
+        jButton1.addActionListener(this);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jTabbedPane1 = new javax.swing.JTabbedPane();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jPanel2 = new javax.swing.JPanel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable2 = new javax.swing.JTable();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTable3 = new javax.swing.JTable();
+        jTextField1 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jTextField3 = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jTextField4 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jTextField5 = new javax.swing.JTextField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        jTabbedPane1.addTab("Tabela", jScrollPane1);
+
+        jTable2.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jTable2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTable2MouseClicked(evt);
+            }
+        });
+        jScrollPane2.setViewportView(jTable2);
+
+        jTable3.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jTable3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTable3MouseClicked(evt);
+            }
+        });
+        jScrollPane3.setViewportView(jTable3);
+
+        jLabel1.setText("Aeroporto 1:");
+
+        jLabel2.setText("Aeroporto 2:");
+
+        jLabel3.setText("Latitude");
+
+        jLabel4.setText("Longitude");
+
+        jLabel5.setText("Latitude");
+
+        jLabel6.setText("Longitude");
+
+        jButton1.setText("Calcular");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(29, 29, 29)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(jButton1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 680, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addGap(18, 18, 18)
+                                .addComponent(jLabel3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 139, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jLabel4)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 139, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 680, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(jLabel2)
+                                .addGap(18, 18, 18)
+                                .addComponent(jLabel5)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(jLabel6)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 139, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(28, 28, 28))))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 166, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 166, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2)
+                    .addComponent(jTextField1)
+                    .addComponent(jTextField3)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel6)
+                    .addComponent(jTextField4))
+                .addGap(31, 31, 31)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(17, 17, 17))
+        );
+
+        jTabbedPane1.addTab("Calculo ", jPanel2);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jTabbedPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 1401, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jTabbedPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 450, Short.MAX_VALUE)
+        );
+
+        jTabbedPane1.getAccessibleContext().setAccessibleName("tabela");
+        jTabbedPane1.getAccessibleContext().setAccessibleDescription("");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jTable2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable2MouseClicked
+        jTextField1.setText(jTable2.getValueAt(jTable2.getSelectedRow(),6).toString());
+        jTextField3.setText(jTable2.getValueAt(jTable2.getSelectedRow(),7).toString());
+    }//GEN-LAST:event_jTable2MouseClicked
+
+    private void jTable3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable3MouseClicked
+        jTextField2.setText(jTable3.getValueAt(jTable3.getSelectedRow(),6).toString());
+        jTextField4.setText(jTable3.getValueAt(jTable3.getSelectedRow(),7).toString());
+    }//GEN-LAST:event_jTable3MouseClicked
+   
+    private static void carregaTabela() {
+
+        List<String[]> valores = new ArrayList<>();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("airports.dat"));
+
+            String header = br.readLine();
+
+            String[] colunas = header.split(",");
+
+            String linha;
+            while ((linha = br.readLine()) != null) {
+
+                String[] indices = linha.split(",");
+                valores.add(indices);
+            }
+
+            br.close();
+
+            DefaultTableModel model = new DefaultTableModel(valores.toArray(Object[][]::new), colunas);
+
+            jTable1.setModel(model);
+                  
+        } catch (Exception e) {
+                
+            e.printStackTrace();       
+        }
+    }
+    
+    @SuppressWarnings("empty-statement")
+    private static void carregaTabelaCalculo() {
+
+        List<String[]> values = new ArrayList<>();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("airports.dat"));
+
+            String header = br.readLine();
+
+            String[] colunas = header.split(",");
+
+            String linha;
+            while ((linha = br.readLine()) != null) {
+
+                String[] ind = linha.split(",");
+                values.add(ind);
+            }
+
+            br.close();
+
+            DefaultTableModel model = new DefaultTableModel(values.toArray(Object[][]::new), colunas);
+
+            jTable2.setModel(model);
+            jTable3.setModel(model);
+                  
+        } catch (Exception e) {
+            
+            e.printStackTrace();            
+        }
+    }
+    
+    private static double haversine(double latitude1, double longitude1,double latitude2, double longitude2){
+        
+        double dLatitude = Math.toRadians(latitude2 - latitude1);
+        double dLongitude = Math.toRadians(longitude2 - longitude1);
+   
+        latitude1 = Math.toRadians(latitude1);
+        latitude2 = Math.toRadians(latitude2);
+        
+        double a = Math.pow(Math.sin(dLatitude / 2), 2)
+                + Math.pow(Math.sin(dLongitude / 2), 2)
+                * Math.cos(latitude1)
+                * Math.cos(latitude2);
+        double radianos = 6371;
+        double c = 2 * Math.asin(Math.sqrt(a));
+        double res =  radianos * c;
+        String str = Double.toString(res);
+        
+        jTextField5.setText(str);
+        return 0;
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private static javax.swing.JPanel jPanel1;
+    private static javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTabbedPane jTabbedPane1;
+    private static javax.swing.JTable jTable1;
+    private static javax.swing.JTable jTable2;
+    private static javax.swing.JTable jTable3;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
+    private static javax.swing.JTextField jTextField5;
+    // End of variables declaration//GEN-END:variables
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        double lat1 = Double.parseDouble(jTextField1.getText());
+        double lon1 = Double.parseDouble(jTextField3.getText());
+        double lat2 = Double.parseDouble(jTextField2.getText());
+        double lon2 = Double.parseDouble(jTextField4.getText());
+        
+        haversine(lat1,lon1,lat2,lon2);
+    }
+    
+     public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new NewJFrame().setVisible(true);
+            
+            carregaTabela();
+            carregaTabelaCalculo(); 
+        });
+    }
+}
---
+++ aeroportos/src/aeroporto/aeroporto.java
@@ -0,0 +1,142 @@
+package aeroporto;
+
+import java.io.Serializable;
+
+public class aeroporto implements Serializable{
+    private String idAirport;
+    private String nameAirport;
+    private String cityAirport;
+    private String countryAirport;
+    private String iataAirport;
+    private String icaoAirport;
+    private String latitudeAirport;
+    private String longitudeAirport;
+    private String altitudeAirport;
+    private String timezoneAirport;
+    private String dstAirport;
+    private String tztimeAirport;
+    private String typeAirport;
+    private String sourceAirport;
+    
+    
+    public aeroporto(){
+    }
+
+    public String getIdAirport() {
+        return idAirport;
+    }
+
+    public void setIdAirport(String idAirport) {
+        this.idAirport = idAirport;
+    }
+
+    public String getNameAirport() {
+        return nameAirport;
+    }
+
+    public void setNameAirport(String nameAirport) {
+        this.nameAirport = nameAirport;
+    }
+
+    public String getCityAirport() {
+        return cityAirport;
+    }
+
+    public void setCityAirport(String cityAirport) {
+        this.cityAirport = cityAirport;
+    }
+
+    public String getCountryAirport() {
+        return countryAirport;
+    }
+
+    public void setCountryAirport(String countryAirport) {
+        this.countryAirport = countryAirport;
+    }
+
+    public String getIataAirport() {
+        return iataAirport;
+    }
+
+    public void setIataAirport(String iataAirport) {
+        this.iataAirport = iataAirport;
+    }
+
+    public String getIcaoAirport() {
+        return icaoAirport;
+    }
+
+    public void setIcaoAirport(String icaoAirport) {
+        this.icaoAirport = icaoAirport;
+    }
+
+    public String getLatitudeAirport() {
+        return latitudeAirport;
+    }
+
+    public void setLatitudeAirport(String latitudeAirport) {
+        this.latitudeAirport = latitudeAirport;
+    }
+
+    public String getLongitudeAirport() {
+        return longitudeAirport;
+    }
+
+    public void setLongitudeAirport(String longitudeAirport) {
+        this.longitudeAirport = longitudeAirport;
+    }
+
+    public String getAltitudeAirport() {
+        return altitudeAirport;
+    }
+
+    public void setAltitudeAirport(String altitudeAirport) {
+        this.altitudeAirport = altitudeAirport;
+    }
+
+    public String getTimezoneAirport() {
+        return timezoneAirport;
+    }
+
+    public void setTimezoneAirport(String timezoneAirport) {
+        this.timezoneAirport = timezoneAirport;
+    }
+
+    public String getDstAirport() {
+        return dstAirport;
+    }
+
+    public void setDstAirport(String dstAirport) {
+        this.dstAirport = dstAirport;
+    }
+
+    public String getTztimeAirport() {
+        return tztimeAirport;
+    }
+
+    public void setTztimeAirport(String tztimeAirport) {
+        this.tztimeAirport = tztimeAirport;
+    }
+
+    public String getTypeAirport() {
+        return typeAirport;
+    }
+
+    public void setTypeAirport(String typeAirport) {
+        this.typeAirport = typeAirport;
+    }
+
+    public String getSourceAirport() {
+        return sourceAirport;
+    }
+
+    public void setSourceAirport(String sourceAirport) {
+        this.sourceAirport = sourceAirport;
+    }
+    
+    
+    
+    
+    
+    
+}
